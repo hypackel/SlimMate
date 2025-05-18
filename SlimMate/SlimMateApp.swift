52201980af0e73fe7e53db5aefcb3f5cefb4a5fb
Hypackel <contact@hypackel.com>
2025-05-18 17:39:04 -0400

    Initial Commit

---
+++ SlimMate/SlimMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimMateApp.swift
+//  SlimMate
+//
+//  Created by hypackel on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SlimMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
